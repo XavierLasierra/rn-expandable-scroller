b57c0e48e6cf0c0e335fe795f1e665b31058a5b1
Xavier Lasierra <xlp1998@gmail.com>
2022-02-15 10:02:28 +0100

    Add mock data

---
+++ src/mocks/elements.ts
@@ -0,0 +1,54 @@
+import { ExpandableItemElement } from "../components/expandableItem/ExpandableItem";
+
+const elements: ExpandableItemElement[] = [
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8238751/pexels-photo-8238751.jpeg?cs=srgb&dl=pexels-hatice-no%C4%9Fman-8238751.jpg&fm=jpg",
+    title: "COFFEE",
+    subtitle: "Is very good",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8117889/pexels-photo-8117889.jpeg?cs=srgb&dl=pexels-kira-schwarz-8117889.jpg&fm=jpg",
+    title: "PLANT",
+    subtitle: "Is green",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/9169378/pexels-photo-9169378.jpeg?cs=srgb&dl=pexels-kira-schwarz-9169378.jpg&fm=jpg",
+    title: "BEACH",
+    subtitle: "Waves",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1526512340740-9217d0159da9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
+    title: "MOUNTAIN",
+    subtitle: "Hike",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/7961716/pexels-photo-7961716.jpeg?cs=srgb&dl=pexels-hatice-no%C4%9Fman-7961716.jpg&fm=jpg",
+    title: "WARDROBE",
+    subtitle: "To save stuff",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8356239/pexels-photo-8356239.jpeg?cs=srgb&dl=pexels-marina-m-8356239.jpg&fm=jpg",
+    title: "LOREM",
+    subtitle: "ipsum ipsum",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/7949032/pexels-photo-7949032.jpeg?cs=srgb&dl=pexels-hatice-no%C4%9Fman-7949032.jpg&fm=jpg",
+    title: "IPSUM",
+    subtitle: "Lorem lorem",
+  },
+  {
+    imageUrl:
+      "https://images.pexels.com/photos/8117885/pexels-photo-8117885.jpeg?cs=srgb&dl=pexels-kira-schwarz-8117885.jpg&fm=jpg",
+    title: "BLACK PLANT",
+    subtitle: "wow",
+  },
+];
+
+export { elements };
